87f894fb95725fe737c35f8715ab45eefe5694a9
Jacob Smith <blasek0@gmail.com>
2016-11-18 16:29:24 -0600

    Revert ""
    This reverts commit f92907837eede1800d2368eb162710f8a8a385d8.

--- src/edu/archer/SettingsMenu.java
+++ src/edu/archer/SettingsMenu.java
@@ -27,7 +27,6 @@ public class SettingsMenu {
     private static double d_SettingsGravityTemp = d_SettingsGravity;
     private static boolean b_SettingsTraceShot;
     private static boolean b_SettingsTraceShotTemp = b_SettingsTraceShot;
-    private static SettingsMenu Settings = null;
     
     //going to get and set the values for player1 and player 2
     ///////////////////////////////////////////////////////////////////////////
@@ -153,6 +152,7 @@ public class SettingsMenu {
         return true; 
     }
     
+    
     public static String getP2Weapon()
     {
         return s_P2Weapon;
@@ -196,79 +196,7 @@ public class SettingsMenu {
     ///////////////////////////////////////////////////////////////////////////
     
     //List of get & set functions for altering settings values
-    
-    public String getBackground()
-    {
-        return s_SettingsBackgroundName;
-    }
-    public int getBackgroundIndex()
-    {
-        String temp = s_SettingsBackgroundName.substring(11,12);
-        //System.out.println(temp);
-        return (Integer.parseInt(temp))-1;
-    }
-    public boolean setBackground(int temp)
-    {
-        switch (temp) {
-            case 0:
-                s_SettingsBackgroundNameTemp = "/backdrops/" + (temp+1) + ".png";
-                //System.out.println(s_SettingsBackgroundName);
-                break;
-            case 1:
-                s_SettingsBackgroundNameTemp = "/backdrops/" + (temp+1) + ".png";
-                //System.out.println(s_SettingsBackgroundName);
-                break;
-            case 2:
-                s_SettingsBackgroundNameTemp = "/backdrops/" + (temp+1) + ".png";
-                //System.out.println(s_SettingsBackgroundName);
-                break;
-            case 3:
-                s_SettingsBackgroundNameTemp = "/backdrops/" + (temp+1) + ".png";
-                //System.out.println(s_SettingsBackgroundName);
-                break;
-            case 4:
-                s_SettingsBackgroundNameTemp = "/backdrops/" + (temp+1) + ".png";
-                //System.out.println(s_SettingsBackgroundName);
-                break;
-            case 5:
-                s_SettingsBackgroundNameTemp = "/backdrops/" + (temp+1) + ".png";
-                //System.out.println(s_SettingsBackgroundName);
-                break;
-            default:
-                s_SettingsBackgroundNameTemp = "/backdrops/1.png";
-                System.out.println("Error");
-        }
-        return true;
-    }
-    
-    public double getGravity()
-    {
-        return d_SettingsGravity;
-    }
-    public boolean setGravity(double temp)
-    {
-        d_SettingsGravityTemp = temp;
-        //System.out.println("Gravity set to: " + d_SettingsGravityTemp);
-        if (d_SettingsGravityTemp == temp) return true;
-        else return false;
-    }
-    public boolean getTraceShotEnabled()
-    {
-        return b_SettingsTraceShot;    
-    }
-    public static boolean setTraceShot(boolean temp)
-    {
-        b_SettingsTraceShotTemp = temp;
-        //System.out.println("Trace Shot set to: " + b_SettingsTraceShotTemp);
-        if (b_SettingsTraceShotTemp == temp) return true;
-        else return false;
-    }
-
-///////////////////////////////////////////////////////////////////////////
-    
-    //Functions for retrieval & storage of settings, constructor, singleton settings handler
-    
-    protected boolean GetInitialSettings()
+    public static void GetInitialSettings()
     {
         try {
             BufferedReader in = new BufferedReader(new FileReader("Settings.txt"));
@@ -303,27 +231,99 @@ public class SettingsMenu {
             in.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(SettingsMenu.class.getName()).log(Level.SEVERE, null, ex);
-            revertToDefaultSettings();
-            return GetInitialSettings();
+            SettingsMenu.revertToDefaultSettings();
+            SettingsMenu.GetInitialSettings();
         } catch (IOException ex) {
             Logger.getLogger(SettingsMenu.class.getName()).log(Level.SEVERE, null, ex);
-            revertToDefaultSettings();
-            return GetInitialSettings();
+            SettingsMenu.revertToDefaultSettings();
+            SettingsMenu.GetInitialSettings();
         } catch (StringIndexOutOfBoundsException ex) {
             Logger.getLogger(SettingsMenu.class.getName()).log(Level.SEVERE, null, ex);
-            revertToDefaultSettings();
-            return GetInitialSettings();
+            SettingsMenu.revertToDefaultSettings();
+            SettingsMenu.GetInitialSettings();
+        }
+    }
+    
+    public static String getBackground()
+    {
+        return s_SettingsBackgroundName;
+    }
+    public static int getBackgroundIndex()
+    {
+        String temp = s_SettingsBackgroundName.substring(11,12);
+        //System.out.println(temp);
+        return (Integer.parseInt(temp))-1;
+    }
+    public static boolean setBackground(int temp)
+    {
+/*        if (temp + ".png" == temp)
+            s_SettingsBackgroundName = temp;
+        else
+            s_SettingsBackgroundName = temp + ".png";
+        if (s_SettingsBackgroundName.equals(temp)) return true;
+        else return false;
+*/        
+        switch (temp) {
+            case 0:
+                s_SettingsBackgroundNameTemp = "/backdrops/" + (temp+1) + ".png";
+                //System.out.println(s_SettingsBackgroundName);
+                break;
+            case 1:
+                s_SettingsBackgroundNameTemp = "/backdrops/" + (temp+1) + ".png";
+                //System.out.println(s_SettingsBackgroundName);
+                break;
+            case 2:
+                s_SettingsBackgroundNameTemp = "/backdrops/" + (temp+1) + ".png";
+                //System.out.println(s_SettingsBackgroundName);
+                break;
+            case 3:
+                s_SettingsBackgroundNameTemp = "/backdrops/" + (temp+1) + ".png";
+                //System.out.println(s_SettingsBackgroundName);
+                break;
+            case 4:
+                s_SettingsBackgroundNameTemp = "/backdrops/" + (temp+1) + ".png";
+                //System.out.println(s_SettingsBackgroundName);
+                break;
+            case 5:
+                s_SettingsBackgroundNameTemp = "/backdrops/" + (temp+1) + ".png";
+                //System.out.println(s_SettingsBackgroundName);
+                break;
+            default:
+                s_SettingsBackgroundNameTemp = "/backdrops/1.png";
+                System.out.println("Error");
         }
         return true;
     }
-    public boolean revertToDefaultSettings()
+    
+    public static double getGravity()
+    {
+        return d_SettingsGravity;
+    }
+    public static boolean setGravity(double temp)
+    {
+        d_SettingsGravityTemp = temp;
+        //System.out.println("Gravity set to: " + d_SettingsGravityTemp);
+        if (d_SettingsGravityTemp == temp) return true;
+        else return false;
+    }
+    public static boolean getTraceShotEnabled()
+    {
+        return b_SettingsTraceShot;    
+    }
+    public static boolean setTraceShot(boolean temp)
+    {
+        b_SettingsTraceShotTemp = temp;
+        if (b_SettingsTraceShotTemp == temp) return true;
+        else return false;
+    }
+    public static void revertToDefaultSettings()
     {
         b_SettingsTraceShotTemp = false;
         d_SettingsGravityTemp = 9.82;
         s_SettingsBackgroundNameTemp = "/backdrops/1.png";
-        return saveSettings();
+        SettingsMenu.saveSettings();
     }
-    public boolean saveSettings()
+    public static void saveSettings()
     {
         s_SettingsBackgroundName = s_SettingsBackgroundNameTemp;
         d_SettingsGravity = d_SettingsGravityTemp;
@@ -339,20 +339,6 @@ public class SettingsMenu {
             out.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(SettingsMenu.class.getName()).log(Level.SEVERE, null, ex);
-            return revertToDefaultSettings();
         }
-        return true;
-    }
-    public static SettingsMenu GetSettings()
-    {
-        if (Settings == null)
-        {
-            Settings = new SettingsMenu();
-        }
-        return Settings;
-    }
-    protected SettingsMenu()
-    {
-        GetInitialSettings();
     }
 }
