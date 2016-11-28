e280a61701b39bf9c126d84c4fe8d342d5ad689c
Jacob Smith <blasek0@gmail.com>
2016-11-28 16:10:53 -0600

    Fixed mismatched combo box labels

--- src/edu/archer/NewGame.java
+++ src/edu/archer/NewGame.java
@@ -90,7 +90,7 @@ public class NewGame extends javax.swing.JFrame {
         });
 
         P1Character.setFont(new java.awt.Font("Arial Black", 1, 14)); // NOI18N
-        P1Character.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Stick Guy", "Hat Guy", "Cowboy", "Batman", "Superman", "Stormtrooper", "Luke", "Roman" }));
+        P1Character.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Stick Guy", "Hat Guy", "Cowboy", "Batman", "Superman", "Stormtrooper", "Luke Skywalker", "Roman" }));
         P1Character.setToolTipText("");
         P1Character.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
